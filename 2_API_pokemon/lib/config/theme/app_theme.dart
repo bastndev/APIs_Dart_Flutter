77ed5183af48d99b7324e3e3dd99ce5a435d0646
bastndev <gohit24@gmail.com>
2023-12-05 21:07:23 -0500

    :alien: Update `API Pokemon`

--- 2_API_pokemon/lib/config/theme/app_theme.dart
+++ 2_API_pokemon/lib/config/theme/app_theme.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class AppTheme {
   ThemeData getTheme() => ThemeData(
         useMaterial3: true,
-        colorSchemeSeed: Colors.white,
+        colorSchemeSeed: Colors.purple,
         appBarTheme: const AppBarTheme(elevation: 1),
       );
 }
