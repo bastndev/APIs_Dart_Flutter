6b7a21e105800092195b7b1e3457582195c7e15f
bastndev <gohit24@gmail.com>
2023-12-05 20:33:32 -0500

    :alien: Update `API Pokemon`

--- 2_API_pokemon/lib/presentation/screens/home_screen.dart
+++ 2_API_pokemon/lib/presentation/screens/home_screen.dart
@@ -1,7 +1,20 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  void initState() {
+    super.initState();
+    getPokemon();
+  }
+
+  Future<void> getPokemon() async {}
 
   @override
   Widget build(BuildContext context) {
